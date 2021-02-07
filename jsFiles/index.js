fef204fd93c0ece542a37193a1b2202f095b5e93
Kajal kumari <50793296+Developers-codz@users.noreply.github.com>
2021-02-07 11:56:18 +0530

    Add files via upload
    this is html templates of home page

---
+++ jsFiles/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    
+   
+$("#top-product .owl-carousel").owlCarousel({
+        nav:true,
+        loop:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    
+
+});
